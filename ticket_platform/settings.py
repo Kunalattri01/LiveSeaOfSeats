871b83df950bd9c0045e58ee620ed1744ca66ba3
Kunalattri01 <kunalattri6224@gmail.com>
2026-05-06 12:17:40 +0530

    Render redis setup

--- ticket_platform/settings.py
+++ ticket_platform/settings.py
@@ -108,15 +108,31 @@ REST_FRAMEWORK = {
 }
 
 
+REDIS_URL = os.getenv(
+    "REDIS_URL",
+    "redis://127.0.0.1:6379/1"
+)
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
+
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 
 WSGI_APPLICATION = 'ticket_platform.wsgi.application'
